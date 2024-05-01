1110bc1070a867819514cbd6ec87d5568c7f6cdd
Liam Nybro <liam.nybro@gmail.com>
2024-05-01 20:31:47 +0200

    Swag ass shit

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-alert("hej");
+const header = document.querySelector("header");
+const menuBtn = document.querySelector("#menu-btn");
+const closeMenuBtn = document.querySelector("#close-menu-btn");
+
+// Togla mobil meny på button click
+menuBtn.addEventListener("click", () => {
+    header.classList.toggle("show-mobile-menu");
+});
+
+//Stäng mobil menu på stäng knappen
+closeMenuBtn.addEventListener("click", () => {
+    menuBtn.click();
+});
